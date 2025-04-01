f66424576cc7cf4374e15aec8b64b9c0c8c87080
francescodelduchetto <francescodelduchetto@gmail.com>
2025-04-01 12:00:15 +0100

    adding connection scripts and setting net=host

--- .devcontainer/run.sh
+++ .devcontainer/run.sh
@@ -11,6 +11,11 @@ set -e
 
 source /opt/pal/gallium/setup.bash
 sudo apt update
+
+# get network functionalities such as ping and ip
+sudo apt install -y iproute2
+sudo apt install -y iputils-ping 
+
 sudo rosdep init
 sudo rosdep update 
 
