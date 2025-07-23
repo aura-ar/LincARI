38add5c9b3fcd9a39480b5a2db0396b77033f149
aura-ar <aurajadeanderson727@gmail.com>
2025-07-23 12:15:38 +0100

    Added  an integrated script for speaking

---
+++ lincari_bringup/scripts/speakingrobot.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import rospy
+
+from actionlib import SimpleActionClient
+from pal_interaction_msgs.msg import TtsAction, TtsGoal
+
+
+def greetings():
+    talking_client = SimpleActionClient("/tts", TtsAction)
+
+    rospy.loginfo("Looking for tts node")
+    talking_client.wait_for_server()
+    rospy.loginfo("Tts Ready!")
+
+    goal = TtsGoal()
+    goal.rawtext.text = "Hello!"
+    goal.rawtext.lang_id = "en_AU"
+    talking_client.send_goal_and_wait(goal)
+    rospy.loginfo("Speech Complete")
+
+def main():
+    rospy.init_node("talking_node", anonymous=True)
+    greetings()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
+
--- lincari_bringup/scripts/wavingrobot.py
+++ lincari_bringup/scripts/wavingrobot.py
@@ -3,29 +3,28 @@
 import rospy
 from actionlib import SimpleActionClient
 from play_motion_msgs.msg import PlayMotionAction, PlayMotionGoal
-
+import speakingrobot
 
 def waving():
 
     client = SimpleActionClient("/play_motion", PlayMotionAction)
 
-    rospy.loginfo("Looking for Node")
+    rospy.loginfo("Looking for Play_Motion Node")
     client.wait_for_server()
-    rospy.loginfo("Node ready!")
+    rospy.loginfo("Play_Motion Node ready!")
 
-    
     goal = PlayMotionGoal()
     goal.motion_name = "wave"
     goal.skip_planning = False
-
+    rospy.loginfo("Executing Movement")
     client.send_goal_and_wait(goal)
 
-    
     rospy.loginfo("Movement Complete")
     
     
 def main():
     rospy.init_node("test_node", anonymous=True)
+    speakingrobot.greetings()
     waving()
 
 
