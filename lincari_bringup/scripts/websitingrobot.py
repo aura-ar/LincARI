fb8b85ae27b666904e3e5dd551ba647ba0fbae5d
aura-ar <aurajadeanderson727@gmail.com>
2025-09-19 16:30:14 +0100

    Deleted all work remembered it all hopefully. If something is broken I forgot to readd it here.

--- lincari_bringup/scripts/websitingrobot.py
+++ lincari_bringup/scripts/websitingrobot.py
@@ -2,7 +2,8 @@
 import rospy
 from pal_web_msgs.msg import WebGoTo
 from pal_interaction_msgs.msg import Input
-import wavingrobot
+from std_msgs.msg import String
+import wavingrobot, speakingrobot
 
 
 
@@ -24,22 +25,40 @@ def publish_touch_page(page_name):
     #rospy.wait_for_service("/user_input")
 
 #Function to happen when the button is detected as pressed
-def callback():
+def callback(txt):
+    global count
+    count = 0
     rospy.loginfo("Button Pressed")
-    wavingrobot.waving()
+    speakingrobot.greetings()
+
+def timesup():
+    pub2 = rospy.Publisher("/touchpage_publisher", String, queue_size=1)
+    rospy.sleep(0.5)
+    signal = "signal"
+    pub2.publish(signal)
+
 
 #Function for listening to the button press and executing previous function
 def listener():
-    
+    global count
     rospy.Subscriber("/user_input", Input, callback)
-    print("Listening")
-    #rospy.spin() because it needs to keep running to detect button presses in the future
-    rospy.spin()
+    while not rospy.is_shutdown():
+        if count < 10:
+            count += 1
+            print(count)
+            rospy.sleep(1)
+            
+        else:
+            timesup()
+            count = 0 
+
     
 #roslaunch rosbridge_server rosbridge_websocket.launch
 
 if __name__ == '__main__':
     try:
+        global count
+        count = 0
         publish_touch_page("auroratesting2")
         print("Published")
         listener()
